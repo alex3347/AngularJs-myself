8a2e49c73600ab85e524978e85e4dd43d8890f7a
Alex <821882886@qq.com>
2017-03-07 21:55:05 +0800

    add php server & modify data constructure

--- rssReader1.1/app.js
+++ rssReader1.1/app.js
@@ -1,19 +1,21 @@
 var app = angular.module("36krDemoApp", []);
-var n = 2;//翻页的计数器
+var n = 2; //翻页的计数器
 app.controller("ajaxXml", function($scope, $http, $timeout) {
+	$jq = $;
 	$scope.LoadXml = function() {
-		$http.get("feed.xml")//涉及到跨域问题，google的rss解析停止服务了，试了下php自己弄解析，发现时间可能不够用，所以这里用了get方法请求本地文件
+		$http.jsonp("http://localhost:8080/rssread.php?callback=JSON_CALLBACK") 
 			.success(function(data) {
+				if(typeof data !== 'object') {
+					var data = JSON.parse(data);
+				}
 				$scope.feeds = [];
-				var rawItems = angular.element(data.trim()).find("item");//找到xml里的item
-				for(var i = 0; i < rawItems.length; i++) {//轮询将每个item的属性提取出来，组成单个对象
-					var feedItem = rawItems.eq(i);
-					linkItem = feedItem.find("link");
+				for(var i = 0; i < data.item.length; i++) { //轮询将每个item的属性提取出来，组成单个对象
+					var feedItem = data.item[i];
 					$scope.feeds.push({
-						title: feedItem.find("title").text().replace(/\<\!\[CDATA\[(.*)\]\]\>/g, '$1'),
-						author: feedItem.find("author").text(),
-						category: feedItem.find("category").text(),
-						publishedDate: feedItem.find("pubDate").html().replace(/\<\!--\[CDATA\[(.+,\s)(\d{2})\s(\w{3})\s(\d{4}\s)(\d{2}:\d{2})(.*)\]\]--\>/g, function($1, $2, $3, $4, $5, $6) {
+						title: feedItem.title,
+						author: feedItem.author,
+						category: feedItem.category,
+						publishedDate: feedItem.pubDate.replace(/(.+,\s)(\d{2})\s(\w{3})\s(\d{4}\s)(\d{2}:\d{2})(.*)/g, function($1, $2, $3, $4, $5, $6) {
 							var engToNum = {
 								Jan: 1,Feb: 2,Mar: 3,Apr: 4,May: 5,Jun: 6,
 								Jul: 7,Aug: 8,Sep: 9,Oct: 10,Nov: 11,Dec: 12
@@ -21,24 +23,24 @@ app.controller("ajaxXml", function($scope, $http, $timeout) {
 							var mon = engToNum[$4];
 							return mon + ' 月 ' + $3 + ' 日 ' + $6;
 						}),
-						contentSnippet: feedItem.find("description").text().substring(0, 54) + '......',
-						imgSrc: feedItem.find("img")[0] == undefined ? 'temp.jpg' : feedItem.find("img")[0].src.split('!')[0],//有些item里没有图片地址，这里我自己加了个本地地址
-						link: linkItem[0].nextSibling.data
+						contentSnippet: $jq(feedItem.description).text().substring(0, 54) + '......',
+						imgSrc: $jq(feedItem.description).find("img")[0] == undefined ? 'temp.jpg' : $jq(feedItem.description).find("img")[0].src.split('!')[0], //有些item里没有图片地址，这里我自己加了个本地地址
+						guid: feedItem.guid
 					});
 				}
 			})
 			.error(function() {
-				alert("读取错误");
+				alert("读取失败");
 			})
 	};
-	$scope.showNum = 10;//每次加载item数量
+	$scope.showNum = 10; //每次加载item数量
 	$scope.$on("BUSY", function() {
-		$scope.busy = true;//true表示加载中...可见
+		$scope.busy = true; //true表示加载中...可见
 	});
 	$scope.$on("NOTBUSY", function() {
 		$scope.busy = false;
 	});
-	$scope.loadMore = function() {//滑动到尾部时加载的函数
+	$scope.loadMore = function() { //滑动到尾部时加载的函数
 		if(n < 11) {
 			$scope.$emit("BUSY");
 			$scope.showNum = 10 * n;
@@ -52,7 +54,7 @@ app.controller("ajaxXml", function($scope, $http, $timeout) {
 		}
 	};
 });
-app.directive('infiniteScroll', ['$rootScope', '$window', '$timeout', function($rootScope, $window, $timeout) {//这个是网上找到的比较成熟的方法，缺点是用了jquery，时间有限，后面我会试着用原生方法自己实现
+app.directive('infiniteScroll', ['$rootScope', '$window', '$timeout', function($rootScope, $window, $timeout) { //这个是网上找到的比较成熟的方法，缺点是用了jquery，时间有限，后面我会试着用原生方法自己实现
 	return {
 		link: function(scope, elem, attrs) {
 			var checkWhenEnabled, handler, scrollDistance, scrollEnabled;
---
+++ rssReader1.1/rssread.php
@@ -0,0 +1,30 @@
+<?php
+header('Content-Type: application/json');
+$feed = new DOMDocument();
+$feed -> load('http://36kr.com/feed');
+$json = array();
+
+$json['title'] = $feed -> getElementsByTagName('channel') -> item(0) -> getElementsByTagName('title') -> item(0) -> firstChild -> nodeValue;
+$json['description'] = $feed -> getElementsByTagName('channel') -> item(0) -> getElementsByTagName('description') -> item(0) -> firstChild -> nodeValue;
+$json['link'] = $feed -> getElementsByTagName('channel') -> item(0) -> getElementsByTagName('link') -> item(0) -> firstChild -> nodeValue;
+
+$items = $feed -> getElementsByTagName('channel') -> item(0) -> getElementsByTagName('item');
+
+$json['item'] = array();
+
+foreach ($items as $item) {
+
+	$author = $item -> getElementsByTagName('author') -> item(0) -> firstChild -> nodeValue;
+	$title = $item -> getElementsByTagName('title') -> item(0) -> firstChild -> nodeValue;
+	$category = $item -> getElementsByTagName('category') -> item(0) -> firstChild -> nodeValue;
+	$guid = $item -> getElementsByTagName('guid') -> item(0) -> firstChild -> nodeValue;
+	$description = $item -> getElementsByTagName('description') -> item(0) -> firstChild -> nodeValue;
+	$pubDate = $item -> getElementsByTagName('pubDate') -> item(0) -> firstChild -> nodeValue;
+
+	$json['item'][] = array("author" => $author, "title" => $title, "category" => $category, "guid" => $guid, "pubDate" => $pubDate, "description" => $description);
+
+}
+$json = json_encode($json);
+$callback = $_GET['callback'];
+exit($callback."($json)");
+?>
